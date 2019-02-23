1abd3f9b02cc86c77a425831ed4ad548e488e1e5
Tim Anema <t.p.d.anema@student.tudelft.nl>
2019-02-23 20:18:09 +0000

    Merge client connection into dev branch

---
+++ client/src/main/java/nl/tudelft/gogreen/api/servermodels/BasicResponse.java
@@ -0,0 +1,12 @@
+package nl.tudelft.gogreen.api.servermodels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasicResponse {
+    private String response;
+}
