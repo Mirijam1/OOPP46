c18ec8ed8f2db3be2c0c9de3f270972617e849d8
Shah Farooq <s.m.farooq@student.tudelft.nl>
2019-03-01 13:20:37 +0100

    Revert "pipeline fix"
    This reverts commit b572a9f2

--- gui/src/main/java/controllers/DashboardController.java
+++ gui/src/main/java/controllers/DashboardController.java
@@ -1,21 +1,38 @@
 package controllers;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.util.StringConverter;
+import nl.tudelft.gogreen.server.models.Activity;
+import nl.tudelft.gogreen.server.models.Category;
+import nl.tudelft.gogreen.server.service.ActivityService;
+import nl.tudelft.gogreen.server.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-//@Component
+import java.util.List;
+
+import static nl.tudelft.gogreen.server.callbacks.CategoryAPI.activitycall;
+import static nl.tudelft.gogreen.server.callbacks.CategoryAPI.categorycall;
+
+@Component
 public class DashboardController {
 
     @FXML
-    private ComboBox<?> categorybox;
+    private ComboBox<Category> categorybox;
 
     @FXML
-    private ComboBox<?> activitybox;
+    private ComboBox<Activity> activitybox;
 
-//    @Autowired
-//    private CategoryService categoryService;
-//    @Autowired
-//    private ActivityService activityService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private ActivityService activityService;
+
+    private List<Category> categoryList;
+    private List<Activity> activityList;
+
 
 
 }
