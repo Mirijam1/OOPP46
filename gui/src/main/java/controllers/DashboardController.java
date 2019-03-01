b572a9f23ecf988a2d5b296947757511db947c96
Shah Farooq <s.m.farooq@student.tudelft.nl>
2019-03-01 02:38:45 +0100

    pipeline fix

--- gui/src/main/java/controllers/DashboardController.java
+++ gui/src/main/java/controllers/DashboardController.java
@@ -1,38 +1,21 @@
 package controllers;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.util.StringConverter;
-import nl.tudelft.gogreen.server.models.Activity;
-import nl.tudelft.gogreen.server.models.Category;
-import nl.tudelft.gogreen.server.service.ActivityService;
-import nl.tudelft.gogreen.server.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-import static nl.tudelft.gogreen.server.callbacks.CategoryAPI.activitycall;
-import static nl.tudelft.gogreen.server.callbacks.CategoryAPI.categorycall;
-
-@Component
+//@Component
 public class DashboardController {
 
     @FXML
-    private ComboBox<Category> categorybox;
+    private ComboBox<?> categorybox;
 
     @FXML
-    private ComboBox<Activity> activitybox;
+    private ComboBox<?> activitybox;
 
-    @Autowired
-    private CategoryService categoryService;
-    @Autowired
-    private ActivityService activityService;
-
-    private List<Category> categoryList;
-    private List<Activity> activityList;
-
+//    @Autowired
+//    private CategoryService categoryService;
+//    @Autowired
+//    private ActivityService activityService;
 
 
 }
