606b407a1db04e2a9400298aa4582c9a49ee888a
Tim Anema <tim.anema@hotmail.nl>
2019-04-01 16:08:00 +0200

    Fix statuscontroller test

--- server/src/test/java/nl/tudelft/gogreen/server/tests/controllers/StatusControllerTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/tests/controllers/StatusControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("tests")
 public class StatusControllerTest {
     @Autowired
     private MockMvc mock;
